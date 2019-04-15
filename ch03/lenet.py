8fc4fe233cfa8a9198e373b4804a16e12077d744
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-04-16 08:34:51 +0900

    MNISTデータの前処理を追加

--- ch03/lenet.py
+++ ch03/lenet.py
@@ -25,3 +25,35 @@ def lenet(input_shape, num_classes):
     model.add(Activation('softmax'))
     
     return model
+
+# MNISTSデータの前処理クラス
+class MNISTDataset():
+    def __init__(self):
+        self.image_shape = (28, 28, 1)  # image is 28x28x1 (grayscale)
+        self.num_classes = 10
+        
+    def get_batch(self):
+        # MNISTデータのロード
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        
+        # 入力データの正規化
+        x_train, x_test = [self.preprocess(d), for d in [x_train, x_test]]
+        
+        # 正解ラベルのバイナリクラスマトリックス化
+        y_train, y_test = [self.preprocess(d, label_data=True) for d in [y_train, y_test]]
+        
+        return x_train, y_train, x_test, y_test
+        
+    def preprocess(self, data, label_data=False):
+        if label_data:
+            # convert class vectors to binary class matrices
+            data = keras.utils.to_categorical(data, self.num_classes)
+        else:
+            data = data.astype('float32')
+            data /= 255 # convert the value to 0-1 scale
+            shape = (data.shape[0],) + self.image_shape # add dataset length to top
+            data = data.reshape(shape)
+            
+        return data
+        
+        
